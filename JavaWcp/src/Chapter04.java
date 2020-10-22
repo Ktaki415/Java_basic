ed31d2a0cffe6110fd3387f4a1f2f5bd19c175a4
瀧村郁 <k.taki415@gmail.com>
2020-10-22 15:40:28 +0900

    [Finish] ex1~5

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str = "瀧村郁";
+		System.out.println(str);
+		
+		str = "同じ阿呆なら踊らにゃ損";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
