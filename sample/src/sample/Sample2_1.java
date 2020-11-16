1c0d55a0538c19e032bbe2c19a066f3e32c865c6
瀧村郁 <k.taki415@gmail.com>
2020-11-16 17:32:55 +0900

    [Fix] ac-tec1

---
+++ sample/src/sample/Sample2_1.java
@@ -0,0 +1,25 @@
+package sample;
+
+public class Sample2_1 {
+	
+	public static void main(String[] args) {
+		
+		// ""でくくられているのは文字列
+		// 文字列を連結したい場合は + 記号を使用する。
+		System.out.println("文字列" + "を" + "連結します");
+		
+		// 計算をする
+		System.out.println(1 + 1);
+		System.out.println(7 - 3);
+		
+		// 文字列を連結し、加算する
+		System.out.println("加算 1 + 1 =" + (1 + 1));
+		
+		//乗算は × ではなく * を使用する
+		System.out.println(2 * 2);
+		
+		//除算は　÷ ではなく / を使用する
+		System.out.println(2 / 2);
+	}
+
+}
